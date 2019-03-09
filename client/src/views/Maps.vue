e2eec8411d41cf27e895ada8a5aae0eaa119647f
Fabio Rodrigues <fabio@superclient.com.br>
2019-03-09 20:09:17 -0300

    feature - tela de reclamacoes

--- client/src/views/Maps.vue
+++ client/src/views/Maps.vue
@@ -2,39 +2,22 @@
   <v-container fill-height fluid>
     <v-layout justify-center wrap>
       <v-flex xs12 md8>
-        <material-card class="v-card-profile">
-          <h4 class="card-title font-weight-light" color="red">Aluguel e compra de casas</h4>
+        <material-card>
           <v-card-text class="text-right">
-            <v-form>
             <v-container py-0>
               <v-layout wrap>
-                <div v-for="mensagem in arrayConversaChat" :key="mensagem.id">
                 <v-flex>
-                  <div v-if="mensagem.tipo === 'texto'">
-                    <v-chip color="red" text-color="white">
-                      <v-avatar color="red darken-4">F</v-avatar>
-                        {{mensagem.texto}}
-                    </v-chip>
+                  <div grid-list-xl> 
+                  <v-select
+                      :items="items"
+                      label="Selecione a categoria da Reclamação"
+                    ></v-select>
                   </div>
-                  <v-flex v-if="mensagem.tipo === 'botao'">
-                    <v-btn small color="red" >
-                      {{mensagem.texto}}
-                    </v-btn>
-                  </v-flex>
-                  <div>
-                    <!-- <v-text-field @click="teste" v-model="resposta" label="Digite uma resposta"></v-text-field> -->
-                  </div>
+                  <v-textarea v-model="teste" label="Descreva com detalhes sua reclamação" xs12></v-textarea>
                 </v-flex>
-                <br>
-                </div>
               </v-layout>
-              <div>
-                <v-btn @click="teste" small color="red">
-                  Prosseguir
-                </v-btn>
-              </div>
+              <div><v-btn small color="red">Prosseguir</v-btn></div>
             </v-container>
-          </v-form>
           </v-card-text>
         </material-card>
       </v-flex>
@@ -46,47 +29,16 @@
 export default {
   data () {
     return {
-      arrayConversaChat: [
-        {
-          id: 0,
-          tipo: 'texto',
-          remetente: 'francis',
-          texto: 'Você gostaria um imóvel para que?'
-        },
-        {
-          id: 0,
-          tipo: 'botao',
-          remetente: 'francis',
-          texto: 'Morar'
-        },
-        {
-          id: 0,
-          tipo: 'botao',
-          remetente: 'francis',
-          texto: 'Meu negócio'
-        },
-        {
-          id: 1,
-          tipo: 'texto',
-          remetente: 'francis',
-          texto: 'Vou te auxiliar hoje!'
-        },
-        {
-          id: 2,
-          tipo: 'texto',
-          remetente: 'francis',
-          texto: 'O que você precisa fazer hoje?'
-        },
-      ],
-      respostasConversa: [],
-      resposta: null
-    }
+      items: [
+        'Problema estrutural',
+        'Problema eletrico',
+        'Pintura'
+        ],
+        teste: null
+      }
   },
   methods: {
-    teste () {
-      this.respostasConversa.push(resposta)
-      console.log('teseee', this.respostasConversa)
-    }
+
   }
 }
 </script>
