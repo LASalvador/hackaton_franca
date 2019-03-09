a28339f607cfb64d1400ddb4384307190220f168
David Gabriel Oliveira <david@superclient.com.br>
2019-03-09 14:44:49 -0300

    Feature - Adicionando array de conversa

---
+++ client/src/views/UserProfileController.js
@@ -0,0 +1,24 @@
+export default {
+  created() {
+  },
+  data() {
+    return {
+      arrayConversaChat: [
+        {
+          id: 0,
+          remetente: 'francis',
+          texto: 'Olá, tudo bem? Eu sou o Francis!'
+        },
+        {
+          id: 1,
+          remetente: 'francis',
+          texto: 'Vou te auxiliar!'
+        },
+
+      ]
+    }
+  },
+  methods: {
+
+  }
+}
