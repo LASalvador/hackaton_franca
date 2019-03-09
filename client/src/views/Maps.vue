0c87dc597435928fbfd6371358534dcddafd2927
Fabio Rodrigues <fabio@superclient.com.br>
2019-03-09 16:25:25 -0300

    feature - tela de aluguel

--- client/src/views/Maps.vue
+++ client/src/views/Maps.vue
@@ -3,11 +3,31 @@
     <v-layout justify-center wrap>
       <v-flex xs12 md8>
         <material-card class="v-card-profile">
-          <v-card-text class="text-xs-center">
-            <h6 class="category text-gray font-weight-thin mb-3">CEO / CO-FOUNDER</h6>
-            <h4 class="card-title font-weight-light">Alec Thompson</h4>
-            <p class="card-description font-weight-light">Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...</p>
-            
+          <h4 class="card-title font-weight-light" color="red">Aluguel e compra de casas</h4>
+          <v-card-text class="text-right">
+            <v-form>
+            <v-container py-0>
+              <v-layout wrap v-for="mensagem in arrayConversaChat" :key="mensagem.id">
+                <v-flex>
+                  <div v-if="mensagem.tipo === 'pergunta'">
+                    <v-chip color="red" text-color="white">
+                      <v-avatar color="red darken-4">F</v-avatar>
+                        {{mensagem.texto}}
+                    </v-chip>
+                  </div>
+                  <div>
+                    <v-text-field @click="teste" v-model="resposta" label="Digite uma resposta"></v-text-field>
+                  </div>
+                </v-flex>
+                <br>
+              </v-layout>
+              <div>
+                <v-btn @click="teste" small color="red">
+                  Prosseguir
+                </v-btn>
+              </div>
+            </v-container>
+          </v-form>
           </v-card-text>
         </material-card>
       </v-flex>
@@ -19,7 +39,34 @@
 export default {
   data () {
     return {
-
+      arrayConversaChat: [
+        {
+          id: 0,
+          tipo: 'pergunta',
+          remetente: 'francis',
+          texto: 'Você gostaria um imóvel para morar ou para o seu negócio?'
+        },
+        {
+          id: 1,
+          tipo: 'pergunta',
+          remetente: 'francis',
+          texto: 'Vou te auxiliar hoje!'
+        },
+        {
+          id: 2,
+          tipo: 'pergunta',
+          remetente: 'francis',
+          texto: 'O que você precisa fazer hoje?'
+        },
+      ],
+      respostasConversa: [],
+      resposta: null
+    }
+  },
+  methods: {
+    teste () {
+      this.respostasConversa.push(resposta)
+      console.log('teseee', this.respostasConversa)
     }
   }
 }
